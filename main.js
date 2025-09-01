c7c465964f3c0272bc237306efad0f589cea126e
Iqblpra_ <iibrrot.15@gmail.com>
2025-09-01 18:15:20 +0700

    Iqbal benerin semua tampilan

---
+++ main.js
@@ -0,0 +1,7 @@
+// Active Nasbar
+  document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+      document.querySelector('.navbar-nav .nav-link.active')?.classList.remove('active');
+      this.classList.add('active');
+    });
+  });
